a7b94ca1ebeb298d717b987ec9b4181b45a2f0cb
HIRO-osaka <hiro.nakamura@pumech.sys.es.osaka-u.ac.jp>
2024-11-08 12:26:24 +0900

    Update gazebo_joint_torsional_spring.cpp

--- src/gazebo_joint_torsional_spring.cpp
+++ src/gazebo_joint_torsional_spring.cpp
@@ -69,7 +69,7 @@ namespace gazebo
 
 		protected: void OnUpdate()
 		{
-			double current_angle = this->joint->GetAngle(0).Radian();
+			double current_angle = this->joint->Position(0);
 			this->joint->SetForce(0, this->kx*(this->setPoint-current_angle));			
 		}
 		
